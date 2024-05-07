7f89aba38f591cbc2e731539491c6cc2897f9ec7
nic11371 <nic11371@gmail.com>
2024-05-07 17:55:48 +0300

    Finished 8-so

--- 8-solution.py
+++ 8-solution.py
@@ -1,10 +1,20 @@
-n = int(input())
-s = []
-maxA = []
-for i in range(n):
-    a = list(map(int, input().split()))
-    s.append(a)
-for j in s:
-    maxA = max(j)
-print(maxA)
-# print(b)
+number = int(input())
+array = []
+maxValues = []
+maxStr = []
+for i in range(number):
+    a = input().split()
+    intValue = []
+    for j in a:
+        b = int(j)
+        intValue.append(b)
+    array.append(intValue)
+for value in array:
+    c = max(value)
+    maxValues.append(c)
+maxValues.sort()
+maxValues.reverse()
+for j in maxValues:
+    d = str(j)
+    maxStr.append(d)
+print(";".join(maxStr))
