508dd011f5daad4dc08ea42be993c22c05c865d2
nic11371 <nic11371@gmail.com>
2024-05-14 12:04:03 +0300

    15-func-with-cache

--- 15-solution.py
+++ 15-solution.py
@@ -1,16 +1,19 @@
-dict = {}
+dictionary = {}
+
+
 def fib(n):
     if n in (1, 2):
         return 1
-    if n in dict.values():
-        return n
-
-    return fib(n - 1) + fib(n - 2)
+    if n in dictionary.keys():
+        return dictionary.get(n)
+    else:
+        val = fib(n - 1) + fib(n - 2)
+        dictionary[n] = val
+        return val
 
 
 num = int(input())
-if 1 <= num <= 30:
+if 1 <= num <= 50:
     print(fib(num))
 else:
     print(-1)
-print(dict)
