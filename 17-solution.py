9e25ed5a8ce64517018c4a077ad82fc9a74f5f98
nic11371 <nic11371@gmail.com>
2024-05-14 21:17:23 +0300

    generator

--- 17-solution.py
+++ 17-solution.py
@@ -1,12 +1,33 @@
-def filter(func, seq) :
+def my_filter(func, seq):
     arr = []
     for i in seq:
-        val = func(i)
-        if val:
-            arr.append(i)
-    return arr
+        if func(i):
+            yield i
+
+
 func_in, seq_in = eval(input()), eval(input())
 
-
-for x in filter(func_in, seq_in):
+for x in my_filter(func_in, seq_in):
     print(x)
+
+# Function filter
+
+# a = filter(lambda x: x < 0, (1, 6, 0, 7, -1, -2, 5))
+#
+# for x in a:
+#     print(x)
+
+# Function without yield
+
+# def my_filter(func, seq):
+#     arr = []
+#     for i in seq:
+#         if func(i):
+#             arr.append(i)
+#     return arr
+#
+#
+# func_in, seq_in = lambda x: x < 0, eval(input())
+#
+# for x in my_filter(func_in, seq_in):
+#     print(x)
