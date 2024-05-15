2d8e3c339c2e2946a608b1192e81d1e36ab69319
nic11371 <nic11371@gmail.com>
2024-05-15 15:42:26 +0300

    LEBG

---
+++ 2-Functions/20-solution.py
@@ -0,0 +1,11 @@
+a = int(input())
+
+
+def f():
+    global a
+    a += 10
+
+
+f()
+
+print(a)
---
+++ 2-Functions/21-solution.py
@@ -0,0 +1,12 @@
+def g():
+    b = int(input())
+
+    def h():
+        nonlocal b
+        b += 10
+
+    h()
+    print(b)
+
+
+g()
